428c43733d9563ff04865972001b102727b26fdd
badconker <badconker@users.noreply.github.com>
2014-01-18 10:36:54 +0100

    fixed issue on expedition report

--- CTRLW.user.js
+++ CTRLW.user.js
@@ -5285,7 +5285,7 @@ Main.k.tabs.expPerma = function() {
 			var heroes = [];
 			var heroes_replace = [];
 			$("ul.adventurers .char").each(function() {
-				var hero = $(this).attr("class").replace("char ", "");
+				var hero = Main.k.GetHeroNameFromTopic($(this).parent());
 				var herof = hero.replace("_", " ").capitalize();
 				heroes_replace.push('<span class="colored_' + hero + '"><img src="/img/icons/ui/' + hero.replace("_", "") + '.png" /> ' + herof + '</span>');
 				heroes.push(herof);
@@ -5293,7 +5293,8 @@ Main.k.tabs.expPerma = function() {
 
 			var html = $(this).html();
 			for (var i=0; i<heroes.length; i++) {
-				html = html.replace(heroes[i], heroes_replace[i]);
+				var regex = new RegExp(heroes[i],'g');
+				html = html.replace(regex, heroes_replace[i]);
 			}
 			$(this).html(html);
 		})
