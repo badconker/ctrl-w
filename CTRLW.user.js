3409515991f36cc2ac44e086488ee99c89903e11
badconker <badconker@users.noreply.github.com>
2014-03-03 18:08:25 +0100

    adds custom styling for messages of andie & derek

--- CTRLW.user.js
+++ CTRLW.user.js
@@ -1326,7 +1326,17 @@ Main.k.css.bubbles = function() {
 		border: 1px solid #5457b0;" + custombubbles_glow + "\
 		padding: 3px 5px! important;\
 	}\
+	.bubble_derek {\
+		background: url(" + Main.k.servurl + "/img/tile_chao.png) center repeat #FFF! important;\
+		border: 1px solid #5457b0;" + custombubbles_glow + "\
+		padding: 3px 5px! important;\
+	}\
 	.bubble_finola {\
+		background: url(" + Main.k.servurl + "/img/tile_finola.png) center repeat #FFF! important;\
+		border: 1px solid #35adbc;" + custombubbles_glow + "\
+		padding: 3px 5px! important;\
+	}\
+	.bubble_andie {\
 		background: url(" + Main.k.servurl + "/img/tile_finola.png) center repeat #FFF! important;\
 		border: 1px solid #35adbc;" + custombubbles_glow + "\
 		padding: 3px 5px! important;\
@@ -1381,7 +1391,9 @@ Main.k.css.bubbles = function() {
 	.bubble_paola span.buddy, .colored_paola { color: #792b70! important; }\
 	.bubble_jin_su span.buddy, .colored_jin_su { color: #a41834! important; }\
 	.bubble_chao span.buddy, .colored_chao { color: #5457b0! important; }\
+	.bubble_derek span.buddy, .colored_derek { color: #5457b0! important; }\
 	.bubble_finola span.buddy, .colored_finola { color: #35adbc! important; }\
+	.bubble_andie span.buddy, .colored_andie { color: #35adbc! important; }\
 	.bubble_kuan_ti span.buddy, .colored_kuan_ti { color: #e89413! important; }\
 	.bubble_ian span.buddy, .colored_ian { color: #647c27! important; }\
 	.bubble_eleesha span.buddy, .colored_eleesha { color: #dca312! important; }\
