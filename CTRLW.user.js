e5691b53d4a87ddc77a8602ae7474f5b034b638e
badconker <badconker@users.noreply.github.com>
2014-11-02 11:43:26 +0100

    Solid skill is now active when Mush

--- CTRLW.user.js
+++ CTRLW.user.js
@@ -330,7 +330,6 @@ Main.k.initData = function() {
 
 	Main.k.compInactiveMush = [];
 	Main.k.compInactiveMush["cold_blood"] = true;
-	Main.k.compInactiveMush["sturdy"] = true;
 	Main.k.compInactiveMush["opportunist"] = true;
 	Main.k.compInactiveMush["optimistic"] = true;
 	Main.k.compInactiveMush["mycologist"] = true;
