6c45b79bd3e5de946b1d94d1a2cc185ef85e2602
badconker <badconker@users.noreply.github.com>
2015-12-14 11:45:41 +0100

    Version 0.36.5

--- src/userscript_metadata.js
+++ src/userscript_metadata.js
@@ -17,5 +17,5 @@
 // @resource    translation:fr translations/fr/LC_MESSAGES/ctrl-w.po
 // @resource    translation:en translations/en/LC_MESSAGES/ctrl-w.po
 // @resource    translation:es translations/es/LC_MESSAGES/ctrl-w.po
-// @version     0.36.5b1
+// @version     0.36.5
 // ==/UserScript==
