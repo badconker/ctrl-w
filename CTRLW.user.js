9c9b178cbb43443ee904b6513166e5f2f2a8c81a
badconker <badconker@users.noreply.github.com>
2016-03-21 18:53:30 +0100

    Version 0.36.7

--- CTRLW.user.js
+++ CTRLW.user.js
@@ -17,7 +17,7 @@
 // @resource    translation:fr translations/fr/LC_MESSAGES/ctrl-w.po
 // @resource    translation:en translations/en/LC_MESSAGES/ctrl-w.po
 // @resource    translation:es translations/es/LC_MESSAGES/ctrl-w.po
-// @version     0.36.6
+// @version     0.36.7
 // ==/UserScript==
 
 var Main = unsafeWindow.Main;
